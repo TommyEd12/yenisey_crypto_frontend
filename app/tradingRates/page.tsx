73f2a18bab1b58cde7fd22eb7ac091b48fea5ecc
egorb <egorbobylev398@gmail.com>
2025-03-05 02:13:25 +0500

    upd

---
+++ app/tradingRates/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const page = () => {
+  return (
+    <main>
+      <div>trading rates</div>
+    </main>
+  );
+};
+
+export default page;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,9 +9,12 @@ export const Navbar = () => {
         <h1>YeniseyCrypto </h1>
       </Link>
       <img src={cryptoIcon.src} className="mt-2 w-10 h-10 bg-transparent" />
-      <section className="ml-3 mt-1 px-2 py-1 bg-zinc-900 flex items-center rounded-2xl">
-        <Link href="/dateInfo" className="text-2xl">
+      <section className="ml-3 mt-1 px-2 py-1  flex items-center ">
+        <Link href="/dateInfo" className="text-2xl px-2 py-1 bg-zinc-900 rounded-2xl">
           Фильтр по дате
+        </Link>
+        <Link href="/tradingRates" className="text-2xl ml-5 px-2 py-1 bg-zinc-900 rounded-2xl">
+          Операции
         </Link>
       </section>
     </div>
